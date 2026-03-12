5c6d94fb19494bd5fb5973fbf722aa12ae086e89
João Paulo <joaopaulo.skodowski@gmail.com>
2026-03-11 22:24:59 -0300

    improve deck.java

--- src/Decks.java
+++ src/Decks.java
@@ -1,76 +1,62 @@
 //src/Decks.java
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-
+import java.util.List;
 
 public class Decks {
-    ArrayList<Card> deck1;
-    ArrayList<Card> deck2;
 
+    private static final String[] SUITS = {"Ouros", "Copas", "Espadas", "Paus"};
+    private static final int[] VALUES = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
-    String[] Suits = {"Ouros", "Copas", "Espadas", "Páus"};
-    int[] Values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-
-    ArrayList<Card> deck = new ArrayList<Card>();
+    private List<Card> deck;
+    private List<Card> deck1;
+    private List<Card> deck2;
 
     public Decks() {
+        deck = new ArrayList<>();
         createDeck();
         shuffleDeck();
         splitDeck();
     }
 
     private void createDeck() {
-
-        for (String suit : Suits) {
-            for (int value : Values) {
-
-                String valueName = getValueName(value);
-                String name = valueName + " de " + suit;
-
-                Card card = new Card(name, suit, value);
-                deck.add(card);
+        for (String suit : SUITS) {
+            for (int value : VALUES) {
+                deck.add(new Card(getValueName(value) + " de " + suit, suit, value));
             }
         }
-
     }
 
     private String getValueName(int value) {
         switch (value) {
-            case 11:
-                return "Valete";
-            case 12:
-                return "Rainha";
-            case 13:
-                return "Rei";
-            case 14:
-                return "Ás";
-            default:
-                return String.valueOf(value);
+            case 11: return "Valete";
+            case 12: return "Rainha";
+            case 13: return "Rei";
+            case 14: return "Ás";
+            default: return String.valueOf(value);
         }
     }
 
-    public ArrayList<Card> getCards() {
-        return deck;
-    }
-
-    private void shuffleDeck(){
-       Collections.shuffle((deck));
+    private void shuffleDeck() {
+        Collections.shuffle(deck);
     }
 
     private void splitDeck() {
         int splitPoint = deck.size() / 2;
 
         deck1 = new ArrayList<>(deck.subList(0, splitPoint));
-
         deck2 = new ArrayList<>(deck.subList(splitPoint, deck.size()));
-
     }
 
-    public ArrayList<Card> getDeck1() {
-        return deck1;
+    public List<Card> getDeck1() {
+        return new ArrayList<>(deck1);
     }
-    public ArrayList<Card> getDeck2() {
-        return deck2;
+
+    public List<Card> getDeck2() {
+        return new ArrayList<>(deck2);
+    }
+
+    public List<Card> getFullDeck() {
+        return new ArrayList<>(deck);
     }
 }
